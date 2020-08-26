ffae9e0a2a5a223c26e859016fd853d6406c0699
Taulant Culi <t.culi91@gmail.com>
2020-08-26 17:04:05 +0200

    first commit and remove of extra code

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="home">
+     <h1>This is an Home Page</h1>
+     <p class="blue--text">Test</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+
+};
+</script>
