e573aff98aa01cacf72e457ebc11f22e405d8253
Taulant Culi <t.culi91@gmail.com>
2020-08-28 17:09:57 +0200

    creating Footer Component and inporting to App

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
   <v-app>
-    <Navbar/>
+    <Navbar />
     <v-main>
       <router-view></router-view>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import Navbar from "@/components/Navbar"
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default {
-  name: 'App',
-  components: {Navbar},
+  name: "App",
+  components: { Navbar, Footer },
   data: () => ({
     //
   }),
---
+++ src/components/Footer.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-footer color="primary lighten-1" padless>
+    <v-row justify="center" no-gutters>
+      <v-card-text flat tile class="white--text text-center">
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-col class="primary lighten-2 py-4 text-center white--text" cols="12">
+        &copy; {{ new Date().getFullYear() }} — <strong>PortSpasy</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    icons: ["mdi-facebook", "mdi-twitter", "mdi-linkedin", "mdi-instagram"],
+  }),
+};
+</script>
